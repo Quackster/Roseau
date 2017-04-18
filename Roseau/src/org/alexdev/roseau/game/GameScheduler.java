df4ba701d05cbccde0846f6196e4c0851951572a
Alex <quacksterr.15@gmail.com>
2017-04-19 02:56:59 +1000

    Remove debug lines

--- Roseau/src/org/alexdev/roseau/game/GameScheduler.java
+++ Roseau/src/org/alexdev/roseau/game/GameScheduler.java
@@ -13,25 +13,22 @@ public class GameScheduler implements Runnable {
 	public void run() {
 		
 		try {
-			Log.println("credits: " + GameVariables.CREDITS_EVERY_SEC);
-			
+
 			List<Player> players = Roseau.getGame().getPlayerManager().getMainServerPlayers();
 			
-			// every 5 seconds
+
 			if ((this.tickRate % GameVariables.CREDITS_EVERY_SEC) == 0) {
 				
 				for (int i = 0; i < players.size(); i++) {
 					
 					Player player = players.get(i);
-					
 					player.getDetails().setCredits(player.getDetails().getCredits() + GameVariables.CREDITS_EVERY_AMOUNT);
 					player.getDetails().sendCredits();
 					player.getDetails().save();
 				}
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Log.exception(e);
 		}
 		
 		this.tickRate++;
